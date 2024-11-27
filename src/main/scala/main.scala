7a9fb2342ad068c0b61c779aeee88ac2af2c8381
Aaditya Joshua Srivastava <16014508@imail.sunway.edu.my>
2024-11-27 18:04:18 +0800

    finished writing main.scala, then when run there was a problem with resource file name, needed to be resources (missing the 's').
    Ran the program, answer returned for questions,
    1. Which state has the highest total hospital beds?
    A) Selangor, 7242101
    
    2. Ratio of dedicated beds for COVID-19 to available beds in dataset?
    A) 0.18
    
    3. Average admission per category (suspected, Covid+, Covid-) per state?
    A)State: Negeri Sembilan, Suspected: 1376.63, COVID-19: 2.78, Non-COVID: 22.03
      State: Kedah, Suspected: 2297.17, COVID-19: 4.73, Non-COVID: 19.86
      State: Terengganu, Suspected: 1429.25, COVID-19: 2.71, Non-COVID: 10.23
      State: Melaka, Suspected: 1190.54, COVID-19: 2.62, Non-COVID: 9.52
      State: W.P. Kuala Lumpur, Suspected: 3177.68, COVID-19: 3.16, Non-COVID: 25.00
      State: Kelantan, Suspected: 2314.08, COVID-19: 4.65, Non-COVID: 23.34
      State: W.P. Labuan, Suspected: 131.54, COVID-19: 0.28, Non-COVID: 0.62
      State: Pulau Pinang, Suspected: 1534.33, COVID-19: 0.72, Non-COVID: 16.20
      State: Selangor, Suspected: 4193.98, COVID-19: 63.20, Non-COVID: 75.09
      State: W.P. Putrajaya, Suspected: 606.58, COVID-19: 6.91, Non-COVID: 4.75
      State: Pahang, Suspected: 1924.07, COVID-19: 2.40, Non-COVID: 14.27
      State: Perak, Suspected: 3794.94, COVID-19: 2.77, Non-COVID: 24.89
      State: Sarawak, Suspected: 3325.35, COVID-19: 28.28, Non-COVID: 54.87
      State: Perlis, Suspected: 485.74, COVID-19: 2.69, Non-COVID: 2.66
      State: Johor, Suspected: 3764.28, COVID-19: 39.51, Non-COVID: 44.85
      State: Sabah, Suspected: 3607.46, COVID-19: 34.35, Non-COVID: 29.57

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,8 +1,8 @@
 import scala.io.Source
-import scala.util.{Try, Using}
+import scala.util.Using
 
 object main{
-  class hospitalDataService(data: List[hospitalData]){
+  private class hospitalDataService(data: List[hospitalData]){
 
     //1. find state with high total hospital bed?
     def stateWithMaxBeds: (String, Int) = {
@@ -28,6 +28,51 @@ object main{
         val avgNonCovid = records.map(_.admittedNonCovid).sum.toDouble/ totalRecords
         (state, (avgPui, avgCovid, avgNonCovid))
       }
+    }
+  }
+
+  //Function to read CSV file from resources
+  private def readData(fileName: String): List[hospitalData]={
+    Using(Source.fromResource(fileName)){ source =>
+      source.getLines().drop(1).map{ line =>
+        val cols = line.split(",")
+        hospitalData(
+          date = cols(0),
+          state = cols(1),
+          beds = cols(2).toInt,
+          bedsCovid = cols(3).toInt,
+          admittedPui = cols(4).toInt,
+          admittedCovid = cols(5).toInt,
+          admittedNonCovid = cols(6).toInt,
+        )
+      }.toList
+    }.getOrElse{
+      println("Error reading the file. Please check & try again")
+      List.empty
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    //Read the hospital data from the CSV file located in resources
+    val hospitalData = readData("hospital.csv")
+
+    //proceed once data is loaded
+    if(hospitalData.nonEmpty){
+      val service = new hospitalDataService(hospitalData)
+
+      val (state, maxBeds) = service.stateWithMaxBeds
+      println(s"State with the highest total hospital beds: $state, Total Beds: $maxBeds")
+
+      val ratio = service.covidBedRatio
+      println(f"Ratio of COVID-19 dedicated beds to total beds: $ratio%.2f")
+
+      val averages = service.avgAdmissionByState
+      println("Average admissions per category per state:")
+      averages.foreach{ case(state, (avgPui, avgCovid, avgNonCovid)) =>
+      println(f"State: $state, Suspected: $avgPui%.2f, COVID-19: $avgCovid%.2f, Non-COVID: $avgNonCovid%.2f")
+      }
+    } else {
+      println("No data available to process")
     }
   }
 }
