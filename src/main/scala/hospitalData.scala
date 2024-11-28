e43b4fae0842ef65eebfe5fe6ac11028fdf444da
Aaditya Joshua Srivastava <16014508@imail.sunway.edu.my>
2024-11-28 16:11:45 +0800

    updated case class to be consistent with hospital.csv

--- src/main/scala/hospitalData.scala
+++ src/main/scala/hospitalData.scala
@@ -1,9 +1,16 @@
 case class hospitalData(
-                       date:String,
-                       state:String,
-                       beds:Int,
-                       bedsCovid:Int,
-                       admittedPui:Int,
-                       admittedCovid:Int,
-                       admittedNonCovid:Int
+                       date:String, //date
+                       state:String, //state names
+                       beds:Int, //total hosp beds
+                       bedsCovid:Int, //total beds for covid19
+                       bedsNonCrit:Int, //total hop beds for non crit
+                       admittedPui:Int, //admitted suspected cases
+                       admittedCovid:Int, //admitted C19+ case
+                       admittedTotal:Int, //admitted individuals
+                       dischargedPui:Int, // discharged suspected cases
+                       dischargedCovid:Int, //discharfed C19+
+                       dischargedTotal:Int, //total discharged
+                       hospCovid:Int, //total C19+ in hosp
+                       hospPui:Int, //total suspected in hosp
+                       hospNonCovid:Int //total non-C19 in hosp
                        )
